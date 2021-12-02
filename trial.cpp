002a2a3e71e932d2effaa5de70ab12333ac88fd8
Iñaki <ipujol10@gmail.com>
2021-12-02 19:45:46 +0100

    UPDATE: make ros work in cpp

--- trial.cpp
+++ trial.cpp
@@ -8,19 +8,21 @@
 #include <string>
 
 int main(int argc, char **argv) {
-//    ros::init(argc, argv, "talker");
-//    ros::NodeHandle n;
-//    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-//    ros::Rate(10);
-//
-//    std_msgs::String message;
-//    std::string str;
-//    std::cout << "Enter message: ";
-//    std::cin >> str;
-//    message.data = str;
-//
-//    ROS_INFO("%s", message.data.c_str());
-//    chatter_pub.publish(message);
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    ros::Rate(10);
+
+    std_msgs::String message;
+    std::string str;
+    std::cout << "Enter message: ";
+    std::cin >> str;
+    message.data = str;
+
+    ROS_INFO("%s", message.data.c_str());
+    chatter_pub.publish(message);
+
+    std::cout << "ok" << std::endl;
 
     return 0;
 }
