997196a2c561a9902d35cf98dd9ad2576989c70a
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-01-10 18:39:54 +0100

    ADD: matlab script break-stand. Constants. #36
    Only added yet the constants from the first_modelV0_2.urdf.xacro

---
+++ hybrid_jumping_robot/Matlab/break_stand.m
@@ -0,0 +1,16 @@
+% Constants
+wheel_r = 0.075;
+wheel_width = 0.03;
+wheel_mass = 0.025;
+base_length = 0.25;
+base_width = 0.15;
+base_height = 0.04;
+base_mass = 0.4;
+extension_length = 0.2;
+extension_width = 0.13;
+extension_height = 0.03;
+extension_mass = 0.4;
+motor_base_length = 0.04;
+motor_base_width = base_width;
+motor_base_height = base_height;
+motor_base_mass = 0.4;
