2669702fd0c5729e592b82fb055a6975f411d69d
Iñaki <ipujol10@gmail.com>
2022-02-09 12:28:10 +0100

    UPDATE: first try at implementing PID #34 Not tested yet

--- hybrid_jumping_robot/scripts/Stabilize.py
+++ hybrid_jumping_robot/scripts/Stabilize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Imu
+from std_msgs.msg import Float64
 from Conversions import quaternion_to_rpy
 import PID
 
@@ -8,15 +9,25 @@ import PID
 class Stabilize:
     orientation = []
 
-    def __init__(self, name, pid=(1, 0, 0)):
+    def __init__(self, name, pid=(1, 0, 0), target=0):
         rospy.init_node(name, anonymous=True)
         rospy.Rate(5)
         self.subscribe()
+        # PUBLISHERS
+        self.send_vel = rospy.Publisher("/internal/stabilize/controller/velocity", Float64, queue_size=10)
+        # PUBLISHERS
         (p, i, d) = pid
         self.pid = PID.PID(p, i, d)
+        self.pid.SetPoint(target)
+        self.pid.setSampleTime(1)
 
     def subscribe(self):
         rospy.Subscriber("/imu", Imu, self.imu_callback)
 
     def imu_callback(self, data: Imu):
         self.orientation = quaternion_to_rpy(data.orientation)
+        (_, pitch, _) = self.orientation  # (roll, pitch, yaw)
+        self.pid.update(pitch)
+        velocity = self.pid.output
+        msg = Float64(velocity)
+        self.send_vel.publish(msg)
