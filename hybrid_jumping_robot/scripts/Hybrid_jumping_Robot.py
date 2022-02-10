6ed424723b6b2e276c0016da3f9836146e9a207a
Mark <mblank16@student.aau.com>
2022-02-10 21:09:32 +0100

    break works!!!!
    Added a couple of functions, to display the velocity
    Changed the mass of the front wheels

--- hybrid_jumping_robot/scripts/Hybrid_jumping_Robot.py
+++ hybrid_jumping_robot/scripts/Hybrid_jumping_Robot.py
@@ -2,24 +2,38 @@
 from pynput import keyboard
 import Robot
 from math import pi
+import os
+
+
+def RadToRpm(radians):
+    return (radians / (2 * pi)) * 60
+
+
+def RpmToRad(rpm):
+    return rpm / 60 * 2 * pi
+
+
+def RpmToVel(rpm, radii):
+    return 2 * pi * radii * rpm
+
 
 class OperatedRobot:
-
     moving = False
+
     def __init__(self, name, velocity):
         self.robot = Robot.Robot(name)
-        self.velocity = (velocity * 2 * pi) / 60  # rotations per minute
+        self.velocity = velocity
         with keyboard.Listener(
                 on_press=self.on_press) as listener:
             listener.join()
 
     def move_robot(self, vel):
         self.robot.set_front_vel(vel)
-        self.robot.set_actuator_position(0.04)
+        self.robot.set_actuator_position(-0.0)
 
     def break_robot(self, vel):
+        self.robot.set_actuator_position(0.00)
         self.robot.set_front_vel(vel)
-        self.robot.set_actuator_position(0.04)
 
     def increase_velocity(self):
         self.velocity += 1.0
@@ -34,24 +48,32 @@ class OperatedRobot:
         if self.moving:
             self.move_robot(-self.velocity)
 
+    # 6.282 sek a rotation
     def on_press(self, key):
-
-        print('Alphanumeric key pressed: {0} '.format(
-            key.char))
-
-        if key.char == 'w':
-            print('move')
-            self.moving = True
-            self.move_robot(-self.velocity)
-        if key.char == 'x':
-            self.moving = False
-            print('break')
-            self.break_robot(0.0)
-        if key.char == '+':
-            self.increase_velocity()
-        if key.char == '-':
-            self.decrease_velocity()
+        try:
+            if key.char == 'w':
+                print('\nmove')
+                self.moving = True
+                print(f'\nVelocity { RpmToVel(RadToRpm(self.velocity),0.075)} cm/minute')
+                self.move_robot(-self.velocity)
+            if key.char == 'x':
+                self.moving = False
+                print('\nbreak')
+                self.break_robot(self.velocity / 50)
+                print(f'\nVelocity { RpmToVel(RadToRpm(0.0),0.075)} cm/minute')
+                self.break_robot(0.0)
+            if key.char == '+':
+                self.increase_velocity()
+                print(f'\nVelocity { RpmToVel(RadToRpm(self.velocity),0.075)} cm/minute')
+            if key.char == '-':
+                self.decrease_velocity()
+                print(f'\nVelocity { RpmToVel(RadToRpm(self.velocity),0.075)} cm/minute')
+            if key.char == '0':
+                print("\nexiting")
+                os._exit(os.EX_OK)
+        except:
+            print(f"\nYou pressed control you moron")
 
 
 if __name__ == '__main__':
-    r = OperatedRobot('hybrid_jumping_robot', 10.0)
+    r = OperatedRobot('hybrid_jumping_robot', 0.0)
