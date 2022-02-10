23983c562776564cf19346756c7a92f65bcbba02
Iñaki <ipujol10@gmail.com>
2022-02-10 10:09:07 +0100

    UPDATE: printout the euler angles #34

--- hybrid_jumping_robot/scripts/Stabilize.py
+++ hybrid_jumping_robot/scripts/Stabilize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Imu
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 from Conversions import quaternion_to_rpy
 import PID
 
@@ -9,25 +9,39 @@ import PID
 class Stabilize:
     orientation = []
 
-    def __init__(self, name, pid=(1, 0, 0), target=0):
-        rospy.init_node(name, anonymous=True)
+    def __init__(self, name, pid=(1, 0, 0), target=0, show: bool = False):
+        rospy.init_node(name, anonymous=True)  # , log_level=rospy.DEBUG)
         rospy.Rate(5)
+        (p, i, d) = pid
+        # self.pid = PID.PID(p, i, d)
+        # self.pid.SetPoint(target)
+        # self.pid.setSampleTime(1)
+        self.show = show
+
         self.subscribe()
         # PUBLISHERS
         self.send_vel = rospy.Publisher("/internal/stabilize/controller/velocity", Float64, queue_size=10)
         # PUBLISHERS
-        (p, i, d) = pid
-        self.pid = PID.PID(p, i, d)
-        self.pid.SetPoint(target)
-        self.pid.setSampleTime(1)
 
     def subscribe(self):
         rospy.Subscriber("/imu", Imu, self.imu_callback)
 
     def imu_callback(self, data: Imu):
-        self.orientation = quaternion_to_rpy(data.orientation)
+        ori = data.orientation
+        self.orientation = quaternion_to_rpy([ori.x, ori.y, ori.z, ori.w])
         (_, pitch, _) = self.orientation  # (roll, pitch, yaw)
-        self.pid.update(pitch)
-        velocity = self.pid.output
-        msg = Float64(velocity)
-        self.send_vel.publish(msg)
+        if self.show:
+            self.show_orientation()
+        # self.pid.update(pitch)
+        # velocity = self.pid.output
+        # msg = Float64(velocity)
+        # self.send_vel.publish(msg)
+
+    def show_orientation(self):
+        rospy.loginfo("(roll, pitch, yaw) = ({}, {}, {})".format(self.orientation[0], self.orientation[1], self.orientation[2]))
+        rospy.loginfo("Pitch = {}".format(self.orientation[1]))
+
+
+if __name__ == "__main__":
+    stable = Stabilize("pid", show=True)
+    rospy.spin()
