147ad682c6acbd13dbb311eb12b95f49b90f3fce
Iñaki <ipujol10@gmail.com>
2022-02-10 10:39:18 +0100

    UPDATE: certify correct euler & be able to rotate launch #34 Update the default target to pi/2 (standing position on the IMU)

--- hybrid_jumping_robot/scripts/Stabilize.py
+++ hybrid_jumping_robot/scripts/Stabilize.py
@@ -4,19 +4,19 @@ from sensor_msgs.msg import Imu
 from std_msgs.msg import Float64, String
 from Conversions import quaternion_to_rpy
 import PID
+import math
 
 
 class Stabilize:
     orientation = []
 
-    def __init__(self, name, pid=(1, 0, 0), target=0, show: bool = False):
+    def __init__(self, name, pid=(1, 0, 0), target=math.pi/2):
         rospy.init_node(name, anonymous=True)  # , log_level=rospy.DEBUG)
         rospy.Rate(5)
         (p, i, d) = pid
         # self.pid = PID.PID(p, i, d)
         # self.pid.SetPoint(target)
         # self.pid.setSampleTime(1)
-        self.show = show
 
         self.subscribe()
         # PUBLISHERS
@@ -30,18 +30,12 @@ class Stabilize:
         ori = data.orientation
         self.orientation = quaternion_to_rpy([ori.x, ori.y, ori.z, ori.w])
         (_, pitch, _) = self.orientation  # (roll, pitch, yaw)
-        if self.show:
-            self.show_orientation()
         # self.pid.update(pitch)
         # velocity = self.pid.output
         # msg = Float64(velocity)
         # self.send_vel.publish(msg)
 
-    def show_orientation(self):
-        rospy.loginfo("(roll, pitch, yaw) = ({}, {}, {})".format(self.orientation[0], self.orientation[1], self.orientation[2]))
-        rospy.loginfo("Pitch = {}".format(self.orientation[1]))
-
 
 if __name__ == "__main__":
-    stable = Stabilize("pid", show=True)
+    stable = Stabilize("pid")
     rospy.spin()
