66b3e3c2d8988af0a6efa032d3eb98b4ca020818
Mark <mblank16@student.aau.com>
2022-02-23 14:19:59 +0100

    PID

--- hybrid_jumping_robot/src/PID_controller.cpp
+++ hybrid_jumping_robot/src/PID_controller.cpp
@@ -3,3 +3,115 @@
 //
 
 #include "PID_controller.hpp"
+
+PID::PID( const std::string &name,
+                                    Float64 KP=0.2,Float64 KI=0.0,Float64 KD=0.0,
+                                    ros::Time current_time= ros::Time{0.0},Float64 sample_time = 0.05){
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("/imu", 1, &PID::imu_callback, this);
+    P = KP;
+    I = KI;
+    D = KD;
+
+    SampleTime = sample_time;
+    CurrentTime = current_time;
+    LastTime = CurrentTime;
+
+    clear();
+
+
+}
+
+void PID::imu_callback(const sensor_msgs::Imu &data) {
+    // TODO: get orientation from quaternion
+}
+
+void PID::clear() {
+    //Clears PID computations and coefficients
+    SetPoint = 0.0;
+
+    PTerm = 0.0;
+    ITerm = 0.0;
+    DTerm = 0.0;
+    LastError = 0.0;
+
+    // Windup Guard
+    Float64 int_error = 0.0;
+    windup_guard = 20.0;
+
+    Output = 0.0;
+}
+
+
+void PID::update(Float64 feedback_value, ros::Time current_time = ros::Time{0.0}) {
+    //Calculates PID value for given reference feedback
+
+    /*..math::
+     * u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+    */
+    Float64 error = SetPoint - feedback_value;
+
+    CurrentTime = ros::Time{current_time};
+
+    ros::Duration delta_time = CurrentTime - LastTime;
+    Float64 delta_error = error - LastError;
+
+    if (delta_time >= ros::Duration{SampleTime})
+    {PTerm = P * error;
+    ITerm += error * delta_time.toSec();
+}
+
+    if (ITerm < -windup_guard){
+    ITerm = -windup_guard;
+}
+    else if(ITerm > windup_guard){
+        ITerm =windup_guard;
+        }
+
+
+    DTerm = 0.0;
+    if (delta_time.toSec() > 0){
+        DTerm = delta_error / delta_time.toSec();
+    }
+
+    //Remember last time and last error for next calculation
+    LastTime = CurrentTime;
+    LastError = error;
+
+    Output = PTerm + (I * ITerm) + (D * DTerm);
+}
+
+
+void PID::setKp(Float64 proportional_gain) {
+    //Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
+   P = proportional_gain;
+}
+void PID::setKi(Float64 integral_gain) {
+    //Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
+   I = integral_gain;
+}
+void PID::setKd(Float64 derivative_gain){
+   //Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
+   D = derivative_gain;
+}
+
+void PID::setWindup(Float64 windup) {
+/*Integral windup, also known as integrator windup or reset windup,
+*refers to the situation in a PID feedback controller where
+*a large change in setpoint occurs (say a positive change)
+*and the integral terms accumulates a significant error
+*        during the rise (windup), thus overshooting and continuing
+* to increase as this accumulated error is unwound
+* (offset by errors in the other direction).
+* The specific problem is the excess overshooting.
+*/
+    windup_guard = windup;
+}
+
+
+void PID::setSampleTime(Float64 sample_t) {
+/* PID that should be updated at a regular interval.
+*  Based on a pre-determined sample time, the PID decides if it should compute or return immediately.
+*/
+    SampleTime = sample_t;
+}
--- hybrid_jumping_robot/src/PID_controller.hpp
+++ hybrid_jumping_robot/src/PID_controller.hpp
@@ -1,8 +1,49 @@
 //
 // Created by woombat on 2/23/22.
 //
-
+#include "Types.hpp"
+#include <ros/ros.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/Imu.h>
+#include <string>
 #ifndef HYBRID_JUMPING_ROBOT_PID_CONTROLLER_HPP
 #define HYBRID_JUMPING_ROBOT_PID_CONTROLLER_HPP
 
 #endif //HYBRID_JUMPING_ROBOT_PID_CONTROLLER_HPP
+class PID{
+
+public:
+    PID( const std::string &name, Float64 KP,Float64 KI,Float64 KD,ros::Time current_time,Float64 sample_time);
+    void clear();
+    void update(Float64 feedback_value, ros::Time current_time);
+    void setSampleTime(Float64 sample_t);
+    void setKp(Float64 proportional_gain);
+    void setKi(Float64 integral_gain);
+    void setKd(Float64 derivative_gain);
+    void setWindup(Float64 windup);
+    void imu_callback(const sensor_msgs::Imu &data);
+
+private:
+    ros::Time CurrentTime;
+    Float64 SampleTime;
+    Float64 SetPoint;
+    ros::Time LastTime;
+    Float64 P;
+    Float64 I;
+    Float64 D;
+    Float64 Target;
+    Float64 LastError;
+    Float64 PTerm;
+    Float64 ITerm;
+    Float64 DTerm;
+    Float64 Output;
+    Float64 windup_guard;
+
+
+};
+
+
+
+
+
