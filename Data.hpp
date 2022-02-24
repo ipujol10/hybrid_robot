c3992b93fccdcbb235ac22bb7ede25e1aea83ae2
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-02-24 10:49:43 +0100

    ADD: Data.hpp

---
+++ Data.hpp
@@ -0,0 +1,9 @@
+#include "Types.hpp"
+#ifndef HYBRID_JUMPING_ROBOT_DATA_HPP
+#define HYBRID_JUMPING_ROBOT_DATA_HPP
+struct RPY {
+  Float64 roll;
+  Float64 pitch;
+  Float64 yaw;
+};
+#endif //HYBRID_JUMPING_ROBOT_DATA_HPP
