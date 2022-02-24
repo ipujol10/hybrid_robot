250546335a0a57ccd6fab05eb02bfc41d94a3fc5
Mark <mblank16@student.aau.com>
2022-02-24 21:10:31 +0100

    ADD: State Machine files
    Only files

---
+++ hybrid_jumping_robot/src/State_machine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by woombat on 2/24/22.
+//
+
+#include "State_machine.hpp"
---
+++ hybrid_jumping_robot/src/State_machine.hpp
@@ -0,0 +1,14 @@
+//
+// Created by woombat on 2/24/22.
+//
+
+#ifndef HYBRID_JUMPING_ROBOT_STATE_MACHINE_HPP
+#define HYBRID_JUMPING_ROBOT_STATE_MACHINE_HPP
+
+
+class State_machine {
+
+};
+
+
+#endif //HYBRID_JUMPING_ROBOT_STATE_MACHINE_HPP
