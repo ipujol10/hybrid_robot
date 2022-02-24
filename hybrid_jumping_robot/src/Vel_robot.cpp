a17b2cc21f3289369304bf553106933334eac8bc
Iñaki <ipujol10@gmail.com>
2022-02-24 11:03:44 +0100

    UPDATE: get the now_velocity #55

--- hybrid_jumping_robot/src/Vel_robot.cpp
+++ hybrid_jumping_robot/src/Vel_robot.cpp
@@ -4,7 +4,8 @@ Vel::Vel() : rate(100) {
   ros::NodeHandle nh;
   left_front_wheel_publisher = nh.advertise<std_msgs::Float64>(left_front_wheel_connection, 10);
   right_front_wheel_publisher = nh.advertise<std_msgs::Float64>(right_front_wheel_connection, 10);
-  ros::Subscriber sub = nh.subscribe("/internal/stabilize/controller/velocity", 1, &Vel::velocity_callback, this);
+  ros::Subscriber vel_sub = nh.subscribe(commanded_velocity_connection, 1, &Vel::velocity_callback, this);
+  ros::Subscriber state_sub = nh.subscribe(joint_state_connection, 1, &Vel::now_vel_callback, this);
 }
 
 void Vel::set_left_front_wheel_velocity(Float64 vel) {
@@ -31,4 +32,8 @@ void Vel::set_front_wheels_velocity(Float64 vel) {
 
 void Vel::velocity_callback(const std_msgs::Float64 &data) {
   set_front_wheels_velocity(data.data);
+}
+
+void Vel::now_vel_callback(const sensor_msgs::JointState &data) {
+  now_velocity = (data.velocity[3] + data.velocity[4]) / 2;
 }
