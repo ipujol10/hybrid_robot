071668f2042b91340c5a12687c2a105ef8d5e46a
Iñaki <ipujol10@gmail.com>
2022-02-27 10:54:03 +0100

    UPDATE: restructure tests #56 Need to see what happens with PID

--- hybrid_jumping_robot/tests/PID/PID_tests.cpp
+++ hybrid_jumping_robot/tests/PID/PID_tests.cpp
@@ -1,17 +1,16 @@
-#include "Simple_control.hpp"
-#include "Mat.hpp"
-#include "../../src/PID_controller.hpp"
 #include <ros/ros.h>
 #include <iostream>
 
-int main() {
-  auto c = Simple_control(0.1);
+#include "PID_tests.hpp"
+
+PID_tests::PID_tests() : c(0.1), pid("pid", 0, 100, 1, 10, 0.01, ros::Time(0)) {}
+
+bool PID_tests::test() {
   std::cout << c.output() << std::endl;
-  auto pid = PID("pid", 0, 100, 1, 10, c.timeStep, ros::Time(0));
   for (int i = 0; i < 100; i++) {
     auto u = pid.update(c.output(), ros::Time((i + 1) * c.timeStep));
     c.update(u);
     std::cout << c.output() << std::endl;
   }
-  return 0;
+  return true;
 }
