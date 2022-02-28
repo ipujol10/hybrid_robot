fa89b8581b3bc2a8b5b12a4dcf12072fd16b2279
Iñaki <ipujol10@gmail.com>
2022-02-28 11:26:01 +0100

    UPDATE: basic calculations #68

--- hybrid_jumping_robot/Calculations/spring.py
+++ hybrid_jumping_robot/Calculations/spring.py
@@ -9,5 +9,15 @@ class SpringCalculation:
     extension_length = 0.2
     motor_base_length = 0.0585
 
-    def __init__(self, minimum_extension=0.1, maximum_extension):
-        a = 1
+    def __init__(self, minimum_extension=0.02, maximum_extension=0.16):
+        if minimum_extension < 0 or minimum_extension > self.extension_length or \
+                maximum_extension > self.extension_length or maximum_extension < minimum_extension:
+            raise ValueError("The maximum and/or minimum extensions are not correct")
+        basic_length = self.base_length + self.motor_base_length
+        self.min_length = basic_length + minimum_extension
+        self.max_length = basic_length + maximum_extension
+
+
+if __name__ == "__main__":
+    spring = SpringCalculation()
+    print("Spring done")
