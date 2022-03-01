0594ece563bc243c18a833e3794dcc833b45c7a8
Mark <mblank16@student.aau.com>
2022-03-01 19:54:42 +0100

    Added logic to state machine node
    Still miss jump, stabelizing ,2 wheel drive,actuation nodes

--- hybrid_jumping_robot/nodes/State_machine_node.cpp
+++ hybrid_jumping_robot/nodes/State_machine_node.cpp
@@ -1,8 +1,7 @@
-#include "IMU_robot.hpp"
+#include "State_machine.hpp"
 #include <ros/ros.h>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "IMU_node");
-    ros::NodeHandle nh;
-    IMU imu;
+    State_machine stateMachine;
 }
