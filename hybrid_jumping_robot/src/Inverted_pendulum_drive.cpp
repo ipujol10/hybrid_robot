15e6ad3f95a14e9cc167542fae929cfd82f85ec4
Iñaki <ipujol10@gmail.com>
2022-03-03 13:36:14 +0100

    UPDATE: kind of stable #62

--- hybrid_jumping_robot/src/Inverted_pendulum_drive.cpp
+++ hybrid_jumping_robot/src/Inverted_pendulum_drive.cpp
@@ -39,7 +39,7 @@ void IPD::loop() {
 int main(int argc, char **argv){
     ros::init(argc, argv,"Inverted_pendulum_drive");
     ros::NodeHandle nh;
-    IPD ipd("pid", M_PI_2, 50, 0.001, 0.01, 1e-2);
+    IPD ipd("pid", M_PI_2, 100, 5e0, 1e-4, 9.9e-3);
     ipd.loop();
     return 0;
 }
