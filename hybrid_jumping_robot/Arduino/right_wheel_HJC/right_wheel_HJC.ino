ddef5fc5f01fcc69d018bd2e1699f5fd75b63ded
woombat84 <mblank16@student.aau.com>
2022-03-10 11:20:47 +0100

    Changed the Dirction and is based on the arduino program for the left wheel

--- hybrid_jumping_robot/Arduino/right_wheel_HJC/right_wheel_HJC.ino
+++ hybrid_jumping_robot/Arduino/right_wheel_HJC/right_wheel_HJC.ino
@@ -1,67 +1,37 @@
 #include <uStepperS.h>
-#include <Wire.h>
-#include <Arduino.h>
 
 #define DEBUG true
 #define MAXACCELERATION 1500  //Max acceleration = 1500 Steps/s^2
 #define MAXVELOCITY 1500      //Max velocity = 1500 steps/s
 
 uStepperS stepper;
-
-int cmd = 0;
+String cmd = "0";
 char msg[4];
 void setup() {
-    /*
-   uint8_t mode = NORMAL,
-       uint16_t stepsPerRevolution = 200,
-       float pTerm = 10.0,
-       float iTerm = 0.0,
-       float dTerm = 0.0,
-       uint16_t dropinStepSize = 16,
-       bool setHome = true,
-       uint8_t invert = 0,
-       uint8_t runCurrent = 50,
-       uint8_t holdCurrent = 30)
- */
-
-    stepper.setup(NORMAL,
-                  200,
-                  10.0,
-                  0.0,
-                  0.0,
-                  16,
-                  true,
-                  0,
-                  50,
-                  30);
+    // put your setup code here, to run once:
+    stepper.setup();
     stepper.runContinous(CW);
-    Serial.begin(9600);
-    Serial1.begin(9600);
+    Serial.begin(115200);
+    Serial1.begin(115200);
 }
 
 void loop() {
 
-    if (0 < Serial1.available()) { // loop through all but the last
-        cmd = Serial1.readBytes(msg, 4);
-        cmd = atoi(msg);
+    if (1 < Serial1.available())
+    {
+        cmd = Serial1.readStringUntil('\n');
         Serial.print("commaded speed is ");
         Serial.println(cmd);
-
+        Serial1.flush();
     }
 
-    if (0 < Serial.available()) { // loop through all but the last
-        cmd = Serial.readBytes(msg, 4);
-        cmd = atoi(msg);
-        Serial.print("commaded speed is ");
-        Serial.println(cmd);
-
-    }
-    if ((cmd == 0)) //Stop
+    if ((cmd == "0")) //Stop
     {
         stepper.stop();
     }
     else {
-        stepper.setRPM(cmd);
+        stepper.setRPM(cmd.toInt());
     }
-    delay(1000);
+    Serial1.println(stepper.encoder.getRPM());
+    delay(5);
 }
