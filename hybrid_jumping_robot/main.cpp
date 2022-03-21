0d7263cca27fee0ff1cfa8192d8f724a05cef2ee
Iñaki <ipujol10@gmail.com>
2022-03-21 10:55:39 +0100

    UPDATE: have ACADO up and running

--- hybrid_jumping_robot/main.cpp
+++ hybrid_jumping_robot/main.cpp
@@ -1,15 +1,10 @@
 #include "src/Hybrid_jumping_Robot.hpp"
-//#include <Eigen/Dense>
+#include <acado/acado_toolkit.hpp>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "hybrid_jumping_robot");
   ros::NodeHandle nh;
-//  Eigen::MatrixXd m(2, 2);
-//  m(0,0) = 3;
-//  m(1,0) = 2.5;
-//  m(0,1) = -1;
-//  m(1,1) = m(1,0) + m(0,1);
-//  std::cout << m << std::endl;
+  ACADO::PIDcontroller pid(2, 1, 0.01);
   auto robot = Hybrid_jumping_Robot("hybrid_jumping_robot", -36, 0.075);
   ros::spin();
 }
