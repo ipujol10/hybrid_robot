7d9c5fbb6a28a1333c4975bef131a13791136322
Iñaki <ipujol10@gmail.com>
2022-03-22 19:46:08 +0100

    UPDATE: kinematics #54

--- hybrid_jumping_robot/src/Kinematics.cpp
+++ hybrid_jumping_robot/src/Kinematics.cpp
@@ -2,9 +2,17 @@
 #include <cmath>
 
 Float64 knm::calculate_Iw() {
-  return mw*pow(R, 2)/2;
+  return mw * pow(R, 2) / 2;
 }
 
-knm::knm(Float64 R, Float64 d, Float64 mb, Float64 mw): R(R), d(d), mb(mb), mw(mw) {
+knm::knm(Float64 R, Float64 d, Float64 mb, Float64 mw) : R(R), d(d), mb(mb), mw(mw) {
   Iw = calculate_Iw();
+  mT = mb + mw;
+}
+
+Float64 knm::f2(Float64 theta, Float64 theta1, Float64 theta2) {
+  auto term1 = R * d * mb * sin(theta) * theta2;
+  auto term2 = R * d * mb * cos(theta) * pow(theta1, 2);
+  auto term3 = pow(R, 2) * mT + Iw;
+  return (term1 - term2) / term3;
 }
--- hybrid_jumping_robot/src/Kinematics.hpp
+++ hybrid_jumping_robot/src/Kinematics.hpp
@@ -7,40 +7,13 @@
 #define g 9.81
 #endif //g
 
-//namespace knm {
-//  /// The function describing the second derivative of the body angle (theta) or the angular acceleration
-//  /// \param R radius of the wheel
-//  /// \param d distance from the wheel center to the center of mass of the body
-//  /// \param m_b mass body
-//  /// \param Ib inertia of the body
-//  /// \param theta the state theta on the work
-//  /// \param phi2 the state phi2 on the work
-//  /// \return the value of the theta2
-//  Float64 f1(Float64 R, Float64 d, Float64 mb, Float64 Ib, Float64 theta, Float64 phi2);
-//
-//  /// The function describing the second derivative of the wheel angle (phi) or the angular acceleration
-//  /// \param R radius of the wheel
-//  /// \param d distance from the wheel center to the center of mass of the body
-//  /// \param mb mass body
-//  /// \param mw mass wheel
-//  /// \param Iw inertia of the wheel
-//  /// \param theta the state theta on the work
-//  /// \param theta1 the state theta1 on the work
-//  /// \param theta2 the state theta2 on the work
-//  /// \return the value of the phi2
-//  Float64 f2(Float64 R, Float64 d, Float64 mb, Float64 mw, Float64 Iw, Float64 theta, Float64 theta1, Float64 theta2);
-//
-//  Float64 calculate_Ib();
-//
-//  Float64 calculate_Iw();
-//}
-
 class knm {
 private:
   Float64 R; //!< radius of the wheel
   Float64 d; //!< distance from the wheel center to the center of mass of the body
   Float64 mb; //!< mass of the body
   Float64 mw; //!< mass of the wheels
+  Float64 mT; //!< total mass of the robot
   Float64 Iw; //!< inertia of the wheel
 
 public:
