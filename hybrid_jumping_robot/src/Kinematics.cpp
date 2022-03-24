2740bea86b549ca905677509d0142e11e6f8cf0a
Iñaki <ipujol10@gmail.com>
2022-03-24 11:40:02 +0100

    UPDATE: tau and friction force #54

--- hybrid_jumping_robot/src/Kinematics.cpp
+++ hybrid_jumping_robot/src/Kinematics.cpp
@@ -1,7 +1,7 @@
 #include "Kinematics.hpp"
 #include <cmath>
 
-Float64 knm::calculate_Iw() {
+Float64 knm::calculate_Iw() const {
   return mw * pow(R, 2) / 2;
 }
 
@@ -10,9 +10,20 @@ knm::knm(Float64 R, Float64 d, Float64 mb, Float64 mw) : R(R), d(d), mb(mb), mw(
   mT = mb + mw;
 }
 
-Float64 knm::f2(Float64 theta, Float64 theta1, Float64 theta2) {
+Float64 knm::f2(Float64 theta, Float64 theta1, Float64 theta2) const {
   auto term1 = R * d * mb * sin(theta) * theta2;
   auto term2 = R * d * mb * cos(theta) * pow(theta1, 2);
   auto term3 = pow(R, 2) * mT + Iw;
   return (term1 - term2) / term3;
+}
+
+Float64 knm::tau(Float64 theta) const {
+  return d * g * mb * cos(theta);
+}
+
+Float64 knm::friction_force(Float64 param, bool theta) const {
+  if (theta) {
+    return tau(param) / R;
+  }
+  return param / R;
 }
--- hybrid_jumping_robot/src/Kinematics.hpp
+++ hybrid_jumping_robot/src/Kinematics.hpp
@@ -35,11 +35,15 @@ public:
   /// \param theta1 the state theta1 on the work
   /// \param theta2 the state theta2 on the work
   /// \return the value of the phi2
-  Float64 f2(Float64 theta, Float64 theta1, Float64 theta2);
+  [[nodiscard]] Float64 f2(Float64 theta, Float64 theta1, Float64 theta2) const;
 
   /// obtain the Inertia of the wheels
   /// \return the inertia
-  Float64 calculate_Iw();
+  [[nodiscard]] Float64 calculate_Iw() const;
+
+  [[nodiscard]] Float64 tau(Float64 theta) const;
+
+  [[nodiscard]] Float64 friction_force(Float64 param, bool theta = true) const;
 };
 
 
