72cb58c6346147ff27ec3aa7d08a4b16a52938d0
Iñaki <ipujol10@gmail.com>
2022-03-25 12:02:57 +0100

    UPDATE: set Q and R #54

--- hybrid_jumping_robot/src/LQR.cpp
+++ hybrid_jumping_robot/src/LQR.cpp
@@ -1,8 +1,10 @@
 #include "LQR.hpp"
+#include <exception>
 
 LQR::LQR() = default;
 
-LQR::LQR(Float64 d, Float64 R, Float64 mw, Float64 mb, Float64 Ib, Float64 point) : kinematics(R, d, mb, mw, Ib) {
+LQR::LQR(Float64 d, Float64 rad, Float64 mw, Float64 mb, Float64 Ib, Float64 point,
+         int qs, int rs) : kinematics(rad, d, mb, mw, Ib), qs(qs), rs(rs), Q(qs, qs), R(rs, rs) {
   A = get_A(point);
   B = get_B(point);
 }
@@ -21,4 +23,24 @@ ACADO::DMatrix LQR::get_B(Float64 theta) const {
   mat(0, 0) = 0;
   mat(1, 0) = kinematics.b(theta);
   return mat;
+}
+
+void LQR::set_Q(const std::vector<Float64> &costs) {
+  if (costs.size() != qs) {
+    throw std::invalid_argument("The costs values don't match the number of states");
+  }
+
+  for (int i = 0; i < qs; i++) {
+    Q(i, i) = costs.at(i);
+  }
+}
+
+void LQR::set_R(const std::vector<Float64> &costs) {
+  if (costs.size() != rs) {
+    throw std::invalid_argument("The costs values don't match the number of inputs");
+  }
+
+  for (int i = 0; i < rs; i++) {
+    R(i, i) = costs.at(i);
+  }
 }
--- hybrid_jumping_robot/src/LQR.hpp
+++ hybrid_jumping_robot/src/LQR.hpp
@@ -8,13 +8,21 @@
 class LQR {
 private:
   ACADO::LinearStateFeedback lqr;
-  ACADO::DMatrix A;
-  ACADO::DMatrix B;
-  KNM kinematics;
+  ACADO::DMatrix A; //!< Linear system
+  ACADO::DMatrix B; //!< Linear system
+  ACADO::DMatrix Q; //!< Cost on the states
+  ACADO::DMatrix R; //!< Cost on the inputs
+  KNM kinematics; //!< kinematics of the system
+  int qs;
+  int rs;
 
 public:
   LQR();
-  LQR(Float64 d, Float64 R, Float64 mw, Float64 mb, Float64 Ib, Float64 point);
+  LQR(Float64 d, Float64 rad, Float64 mw, Float64 mb, Float64 Ib, Float64 point, int qs = 2, int rs = 1);
+
+  void set_Q(const std::vector<Float64> &costs);
+
+  void set_R(const std::vector<Float64> &costs);
 
 private:
   [[nodiscard]] ACADO::DMatrix get_A(Float64 theta) const;
