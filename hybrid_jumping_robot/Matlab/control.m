2b8bfc78184e4e2fac5cdbce4d8b327e9a0eb2f5
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-03-30 12:54:47 +0200

    UPDATE: start simulink #54

--- hybrid_jumping_robot/Matlab/control.m
+++ hybrid_jumping_robot/Matlab/control.m
@@ -10,8 +10,8 @@ A = double(subs(Ap, parameters, values));
 B = double(subs(Bp, parameters, values));
 
 %% Output
-C = [1 0 0 0; 0 1 0 0]; % get theta and phi as the output
-% C = [0 1 0 0];
+% C = [1 0 0 0; 0 1 0 0]; % get theta and phi as the output
+C = [0 1 0 0];
 % C = [1 0 0 0];
 D = 0;
 
@@ -25,8 +25,34 @@ So = obsv(sys);
 if (~(rank(Sc) == 4 && rank(So) == 4))
     disp("Can't control or/and observe");
 else
-    if (size(C, 1) == 1)
-        rlocus(sys);
-    end
+%     if (size(C, 1) == 1)
+%         rlocus(sys);
+%     end
+    x0 = [pi/2+5*pi/180 0 0 0]';
+%     x0 = [pi/2 0 0 0]';
     
+    %% Control
+%     poles = [-3; -3; -3; -3]*.5;
+%     K = acker(A, B, poles);
+    Q = [1 0 0 0; 0 0.001 0 0; 0 0 0.001 0; 0 0 0 0.001];
+    R = .1;
+    K = lqr(A, B, Q, R);
+    
+    %% Descrete
+    
+    
+    
+    %% Simulate
+    out = sim("model");
+    
+    %% Figures
+    figure;
+    plot(out.x);
+    title("x");
+    lefend = legend("$\theta$", "$\phi$", "$\dot{\theta}$", "$\dot{\phi}$");
+    set(legend, 'Interpreter','latex');
+    
+    figure;
+    plot(out.u);
+    title("u");
 end
