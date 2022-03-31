8b83aa40e2bdcb7a8030fa7323de9efbf80f6d31
Iñaki <ipujol10@gmail.com>
2022-03-31 13:01:31 +0200

    UPDATE: delete Kinematics.cpp #54

--- hybrid_jumping_robot/src/LQR.hpp
+++ hybrid_jumping_robot/src/LQR.hpp
@@ -1,35 +1,18 @@
 #ifndef HYBRID_JUMPING_ROBOT_LQR_HPP
 #define HYBRID_JUMPING_ROBOT_LQR_HPP
 
-#include "Kinematics.hpp"
+#include "Types.hpp"
 #include <acado_toolkit.hpp>
 
 
 class LQR {
 private:
-  ACADO::LinearStateFeedback lqr;
   ACADO::DMatrix A; //!< Linear system
   ACADO::DMatrix B; //!< Linear system
-  ACADO::DMatrix Q; //!< Cost on the states
-  ACADO::DMatrix R; //!< Cost on the inputs
-  unsigned int xs; //!< number of states
-  unsigned int us; //!< number of inputs
+  ACADO::DMatrix K; //!< The control Matrix
 
 public:
   LQR();
-  LQR(Float64 d, Float64 rad, Float64 mw, Float64 mb, Float64 Ib, Float64 point, int qs = 2, int rs = 1);
-  LQR(const ACADO::DMatrix &A, const ACADO::DMatrix &B);
-
-  ACADO::DMatrix Riccati(const std::vector<Float64> &state_costs, const std::vector<Float64> &input_costs);
-
-private:
-  void set_Q(const std::vector<Float64> &costs);
-
-  void set_R(const std::vector<Float64> &costs);
-
-  [[nodiscard]] ACADO::DMatrix get_A(Float64 theta, const KNM &kinematics) const;
-
-  [[nodiscard]] ACADO::DMatrix get_B(Float64 theta, const KNM &kinematics) const;
 };
 
 
