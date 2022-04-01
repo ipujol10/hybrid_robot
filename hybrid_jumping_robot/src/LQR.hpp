008fd73a82aef925884a0cefe766b799ebd20799
Iñaki <ipujol10@gmail.com>
2022-04-01 11:43:49 +0200

    UPDATE: the attributes of the lqr #54

--- hybrid_jumping_robot/src/LQR.hpp
+++ hybrid_jumping_robot/src/LQR.hpp
@@ -2,6 +2,7 @@
 #define HYBRID_JUMPING_ROBOT_LQR_HPP
 
 #include "Types.hpp"
+#include "Observer.hpp"
 #include <acado_toolkit.hpp>
 
 
@@ -11,6 +12,8 @@ private:
   ACADO::DMatrix B; //!< Linear system
   ACADO::DMatrix K; //!< The control Gain
   ACADO::DMatrix KObs; //!< The observer Gain
+  Observer observer; //!< The observer to estimate the states
+  ACADO::LinearStateFeedback lqr; //!< LQR controller
 
 public:
   LQR();
