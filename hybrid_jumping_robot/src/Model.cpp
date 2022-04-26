fb70f7740378b1ffb3e9976ad6cd6d949fe8c419
Iñaki <ipujol10@gmail.com>
2022-04-26 11:42:31 +0200

    UPDATE: initial model implementation #89

--- hybrid_jumping_robot/src/Model.cpp
+++ hybrid_jumping_robot/src/Model.cpp
@@ -1,1 +1,48 @@
 #include "Model.hpp"
+
+Model::Model(int states, int inputs, int outputs) : X(states), U(inputs), Y(outputs) {
+  A = Eigen::MatrixXd(X, X);
+  B = Eigen::MatrixXd(X, U);
+  C = Eigen::MatrixXd(Y, X); /*NOLINT*/
+  D = Eigen::MatrixXd(Y, U);
+}
+
+void Model::set_model(const Eigen::MatrixXd &a, const Eigen::MatrixXd &b, const Eigen::MatrixXd &c,
+                      const Eigen::MatrixXd &d) {
+  if (!(a.rows() == a.cols() && a.rows() == 1 && a(0, 0) == 0)) {
+    this->A = a;
+  }
+  if (!(b.rows() == b.cols() && b.rows() == 1 && b(0, 0) == 0)) {
+    this->B = b;
+  }
+  if (!(c.rows() == c.cols() && c.rows() == 1 && c(0, 0) == 0)) {
+    this->C = c;
+  }
+  if (!(d.rows() == d.cols() && d.rows() == 1 && d(0, 0) == 0)) {
+    this->D = d;
+  }
+}
+
+void Model::set_A(const Eigen::MatrixXd &a) {
+  if (!(a.rows() == a.cols() && a.rows() == 1 && a(0, 0) == 0)) {
+    this->A = a;
+  }
+}
+
+void Model::set_B(const Eigen::MatrixXd &b) {
+  if (!(b.rows() == b.cols() && b.rows() == 1 && b(0, 0) == 0)) {
+    this->B = b;
+  }
+}
+
+void Model::set_C(const Eigen::MatrixXd &c) {
+  if (!(c.rows() == c.cols() && c.rows() == 1 && c(0, 0) == 0)) {
+    this->C = c;
+  }
+}
+
+void Model::set_D(const Eigen::MatrixXd &d) {
+  if (!(d.rows() == d.cols() && d.rows() == 1 && d(0, 0) == 0)) {
+    this->D = d;
+  }
+}
--- hybrid_jumping_robot/src/Model.hpp
+++ hybrid_jumping_robot/src/Model.hpp
@@ -1,9 +1,32 @@
 #ifndef HYBRID_JUMPING_ROBOT_MODEL_HPP
 #define HYBRID_JUMPING_ROBOT_MODEL_HPP
 
+#include <Eigen/Dense>
 
 class Model {
+private:
+  const int X; //!< number of states
+  const int U; //!< number of inputs
+  const int Y; //!< number of outputs
 
+  Eigen::MatrixXd A; //!< A matrix of the system
+  Eigen::MatrixXd B; //!< B matrix of the system
+  Eigen::MatrixXd C; //!< C matrix of the system
+  Eigen::MatrixXd D; //!< D matrix of the system
+
+public:
+  Model(int states, int inputs, int outputs);
+
+  void
+  set_model(const Eigen::MatrixXd &a, const Eigen::MatrixXd &b, const Eigen::MatrixXd &c, const Eigen::MatrixXd &d);
+
+  void set_A(const Eigen::MatrixXd &a);
+
+  void set_B(const Eigen::MatrixXd &b);
+
+  void set_C(const Eigen::MatrixXd &c);
+
+  void set_D(const Eigen::MatrixXd &d);
 };
 
 
