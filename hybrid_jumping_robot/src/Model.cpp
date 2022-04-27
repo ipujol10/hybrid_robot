b1ea4ce2607043160f6a1d0cb607e7d5f97f0df8
Iñaki <ipujol10@gmail.com>
2022-04-27 12:10:01 +0200

    UPDATE: make sure that A and C have to be introduced #89

--- hybrid_jumping_robot/src/Model.cpp
+++ hybrid_jumping_robot/src/Model.cpp
@@ -11,24 +11,23 @@ Model::Model() = default;
 
 void Model::set_model(const Matrix &a, const Matrix &b, const Matrix &c,
                       const Matrix &d) {
-  if (!(a.rows() == a.cols() && a.rows() == 1 && a(0, 0) == 0)) {
-    if (a.rows() != X || a.cols() != X) {
-      throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
-    }
-    this->A = a;
+  if (a.rows() != X || a.cols() != X) {
+    throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
   }
+  this->A = a;
+
   if (!(b.rows() == b.cols() && b.rows() == 1 && b(0, 0) == 0)) {
     if (b.rows() != X || b.cols() != U) {
       throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
     }
     this->B = b;
   }
-  if (!(c.rows() == c.cols() && c.rows() == 1 && c(0, 0) == 0)) {
-    if (c.rows() != Y || c.cols() != X) {
-      throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
-    }
-    this->C = c;
+
+  if (c.rows() != Y || c.cols() != X) {
+    throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
   }
+  this->C = c;
+
   if (!(d.rows() == d.cols() && d.rows() == 1 && d(0, 0) == 0)) {
     if (d.rows() != Y || d.cols() != U) {
       throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
@@ -38,12 +37,10 @@ void Model::set_model(const Matrix &a, const Matrix &b, const Matrix &c,
 }
 
 void Model::set_A(const Matrix &a) {
-  if (!(a.rows() == a.cols() && a.rows() == 1 && a(0, 0) == 0)) {
-    if (a.rows() != X || a.cols() != X) {
-      throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
-    }
-    this->A = a;
+  if (a.rows() != X || a.cols() != X) {
+    throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
   }
+  this->A = a;
 }
 
 void Model::set_B(const Matrix &b) {
@@ -56,12 +53,10 @@ void Model::set_B(const Matrix &b) {
 }
 
 void Model::set_C(const Matrix &c) {
-  if (!(c.rows() == c.cols() && c.rows() == 1 && c(0, 0) == 0)) {
-    if (c.rows() != Y || c.cols() != X) {
-      throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
-    }
-    this->C = c;
+  if (c.rows() != Y || c.cols() != X) {
+    throw std::invalid_argument("The dimensions of the matrix doesn't match with the model");
   }
+  this->C = c;
 }
 
 void Model::set_D(const Matrix &d) {
