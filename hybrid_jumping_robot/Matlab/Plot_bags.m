90df6b851bc5614bf7a6435c5bbd68284565e796
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-05-23 09:53:20 +0200

    UPDATE: better IMU plot
    Have all the info on a single plot and take a constant time value

--- hybrid_jumping_robot/Matlab/Plot_bags.m
+++ hybrid_jumping_robot/Matlab/Plot_bags.m
@@ -9,22 +9,37 @@ imu_bags = [imu_bags rosbag("../bagfiles/IMU_test/90d.bag")];
 imu_bags = [imu_bags rosbag("../bagfiles/IMU_test/135d.bag")];
 imu_bags = [imu_bags rosbag("../bagfiles/IMU_test/180d.bag")];
 
+shortest_time = imu_bags(1).EndTime - imu_bags(1).StartTime;
+
 titles = ["0 deg", "45 deg", "90 deg", "135 deg", "180 deg"];
 
+imu_ts = zeros(size(imu_bags));
+
+for i = 2:size(imu_bags, 2)
+    current_time = imu_bags(i).EndTime - imu_bags(i).StartTime;
+    if current_time < shortest_time
+        shortest_time = current_time;
+    end
+end
+
+figure
+hold on
 for i = 1:size(imu_bags, 2)
-    imu_angle = select(imu_bags(i), "Topic", "/HJC/IMU/Pitch");
+    imu_angle = select(imu_bags(i), "Time", [imu_bags(i).StartTime, imu_bags(i).StartTime + shortest_time], "Topic", "/HJC/IMU/Pitch");
     imu_ts = timeseries(imu_angle, "Data");
     imu_ts.Time = imu_ts.Time - imu_ts.Time(1,1)*ones(size(imu_ts));
     imu_ts.Data = imu_ts.Data*180/pi;
-    subplot(3, 2, i)
     plot(imu_ts)
-    title(titles(i))
-    ylabel("Pitch [º]")
     imu_stat(i).name = titles(i);
     imu_stat(i).mean = mean(imu_ts);
     imu_stat(i).max = max(imu_ts);
     imu_stat(i).min = min(imu_ts);
 end
+legend(titles)
+ylabel("Pitch [º]")
+xlabel("Time [s]")
+title("Pitch tests")
+hold off
 
 %% Odometry
 figure
