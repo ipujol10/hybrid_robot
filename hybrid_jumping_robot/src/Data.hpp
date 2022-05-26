5133c6523b8876b83a39dc06e80439b4a2ab3135
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2022-05-26 11:31:38 +0200

    UPDATE: add correct pole placement in discrete time

--- hybrid_jumping_robot/src/Data.hpp
+++ hybrid_jumping_robot/src/Data.hpp
@@ -13,6 +13,7 @@ enum class System {
   pole_placement, // poles = {0, -1, -3, -6.87565805306348}
   k_manual,
   tuned_manual,
-  LQR
+  LQR,
+  pole_placement_discrete // poles = {0.932884813030418 +   0.00310411766981547i, 0.932884813030418 -   0.00310411766981547i, 0.999841011936642 +  0.000158914166594503i, 0.999841011936642 -  0.000158914166594503i}
 };
 #endif //HYBRID_JUMPING_ROBOT_DATA_HPP
